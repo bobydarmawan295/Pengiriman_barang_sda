e05d2683203f386e40c3ee4d43682abf933d2f76
Boby Darmawan <90325160+bobydarmawan295@users.noreply.github.com>
2022-03-29 15:00:30 +0700

    Update App.java

--- src/Barang/App.java
+++ src/Barang/App.java
@@ -14,8 +14,9 @@ public class App {
         Scanner input = new Scanner(System.in);
         ArrayList <String> list_komen = new ArrayList <>();
         boolean isContinue = true;
-        boolean isContinue2 = true;
-        boolean isContinue3 = true;
+        boolean isContinue1;
+        boolean isContinue2;
+        boolean isContinue3;
         String pilihanUser;
         User user = new User();
         Barang barang = new Barang();
@@ -34,129 +35,140 @@ public class App {
             4. Tree
             5. Keluar
             """);
-
+            isContinue1 =true;
+            isContinue2 = true;
+            isContinue3 = true;
             System.out.print("Input pilihan : ");
             String pil1 = input.next();
             
             switch(pil1){
                 case "1":
-                        System.out.println("===============================================");
-                        System.out.println("               Menu pelanggan                  ");
-                        System.out.println("===============================================");
-                        System.out.println("""
-                        \n1.Masukkan identitas pengirim dan penerima
-                        2.Lihat pelanggan
-                        3.Hapus pelanggan
-                        """);
-                        System.out.print("Pilihan : ");
-                        pilihanUser = input.next();
-                        switch(pilihanUser){
-                            case"1":
-                                user.enqUser();
-                                break;
-                            case "2":
-                                user.viewUser();
-                                break;
-                            case "3":
-                                user.deqUser();
-                                break;
-                            default:
-                                System.out.println("Pilihan tidak tersedia");
-                                break;
-
-                        }              
+                        while(isContinue1){
+                            System.out.println("===============================================");
+                            System.out.println("               Menu pelanggan                  ");
+                            System.out.println("===============================================");
+                            System.out.println("""
+                            \n1.Masukkan identitas pengirim dan penerima
+                            2.Lihat pelanggan
+                            3.Hapus pelanggan
+                            """);
+                            System.out.print("Pilihan : ");
+                            pilihanUser = input.next();
+                            switch(pilihanUser){
+                                case"1":
+                                    user.enqUser();
+                                    break;
+                                case "2":
+                                    user.viewUser();
+                                    break;
+                                case "3":
+                                    user.deqUser();
+                                    break;
+                                default:
+                                    System.out.println("Pilihan tidak tersedia");
+                                    break;
+                            }
+                            System.out.print("\nApakah Anda ingin melanjutkan [y/n]? ");
+                            pilihanUser = input.next();
+                            isContinue1 = pilihanUser.equalsIgnoreCase("y");
+                        
+                        }
                     break;
                 case "2":
                     if(user.isEmpty()){
                         System.out.println("Silahkan isi data pelanggan dulu");
                     }else{
-                        System.out.println("===============================================");
-                        System.out.println("               Menu Barang                   ");
-                        System.out.println("===============================================");
-                        System.out.println("""
-                        1.Tampil barang
-                        2.Tambah Belakang
-                        3.Hapus Belakang
-                        4.Push Barang
-                        5.Pop Barang
-                        6.Kirim barang
-                        """);
-                        System.out.print("Masukkan pilihan : ");
-                        pilihanUser = input.next();
-                        
-                        switch(pilihanUser){
-                            case "1":
-                                barang.viewBarang();
-                                break;
-                            case "2":
-                                barang.tambahBelakang();
-                                break;
-                            case "3":
-                                barang.hapusBelakang();
-                                break;
-                            case "4":
-                                barang.pushBarang();
-                                break;
-                            case "5":
-                                barang.popBarang();
-                                break;
-                            case "6":
-                                System.out.println("Pengiriman barang dalam proses");
-                                break;
-                            default:
-                                System.out.println("Pilihan tidak tersedia");
-                                break;
-                        }
-                        
+                        while(isContinue2){
+                            System.out.println("===============================================");
+                            System.out.println("               Menu Barang                   ");
+                            System.out.println("===============================================");
+                            System.out.println("""
+                            1.Tampil barang
+                            2.Tambah Belakang
+                            3.Hapus Belakang
+                            4.Push Barang
+                            5.Pop Barang
+                            6.Kirim barang
+                            """);
+                            System.out.print("Masukkan pilihan : ");
+                            pilihanUser = input.next();
+                            
+                            switch(pilihanUser){
+                                case "1":
+                                    barang.viewBarang();
+                                    break;
+                                case "2":
+                                    barang.tambahBelakang();
+                                    break;
+                                case "3":
+                                    barang.hapusBelakang();
+                                    break;
+                                case "4":
+                                    barang.pushBarang();
+                                    break;
+                                case "5":
+                                    barang.popBarang();
+                                    break;
+                                case "6":
+                                    System.out.println("Pengiriman barang dalam proses");
+                                    break;
+                                default:
+                                    System.out.println("Pilihan tidak tersedia");
+                                    break;
+                            }
                             System.out.print("\nApakah Anda ingin melanjutkan [y/n]? ");
                             pilihanUser = input.next();
                             isContinue2 = pilihanUser.equalsIgnoreCase("y");
+                        }       
                     }
                         break;
                 case "3":            
                         if(user.isEmpty() && barang.isEmpty()){
                             System.out.println("Silahkan isi data pelanggan dan barang terlebih dahulu (^ ^)");
                         }else{
-                            System.out.println("Feedback Penerima");
-                            System.out.println("-----------------");
-                            System.out.print("Menu : \n1. Lihat ulasan\n2. Beri ulasan\n\nPilih :");
-                            int pilihan = input.nextInt();
-                            switch(pilihan){
-                            case 1:
-                                if (list_komen.isEmpty()){
-                                System.out.println("Tidak ada ulasan");
-                                }
-                                else{
-                                    for(int d = 0;d < list_komen.size();d++){
-                                        System.out.println(list_komen.get(d)+"\n");
+                            while(isContinue3){
+                                System.out.println("-----------------");
+                                System.out.println("Feedback Penerima");
+                                System.out.println("-----------------");
+                                System.out.print("Menu : \n1. Lihat ulasan\n2. Beri ulasan\n\nPilih :");
+                                int pilihan = input.nextInt();
+                                switch(pilihan){
+                                case 1:
+                                    if (list_komen.isEmpty()){
+                                    System.out.println("Tidak ada ulasan");
+                                    }
+                                    else{
+                                        for(int d = 0;d < list_komen.size();d++){
+                                            System.out.println(list_komen.get(d)+"\n");
+                                        }
+                                        break;
                                     }
                                     break;
+
+                                    case 2:
+                                    System.out.print("\nMasukkan Nama: ");
+                                    String name = input.next();
+                                    System.out.println("Nama: "+name);
+                                    System.out.println("\nPenilaian Pelayanan: ");
+                                    System.out.println("1\t2\t3\t4\t5");
+                                    System.out.print("\nNilai: ");
+                                    Integer pilih = input.nextInt();
+                                    System.out.print("\nMasukkan Komentar: ");
+                                    String komen = input.next();
+                                    list_komen.add("\nNama\t\t: "+name+"\nPenilaian\t:"+pilih+"\nKomentar\t: "+komen);
+                                    System.out.println("\nNama\t\t: "+name+"\nPenilaian\t: "+pilih+"\nKomentar\t: "+komen);
+                                    break;
+
+                                    default:
+                                    System.out.println("Pilihan tidak tersedia");
+                                    break;
                                 }
-                                break;
-
-                                case 2:
-                                System.out.print("\nMasukkan Nama: ");
-                                String name = input.next();
-                                System.out.println("Nama: "+name);
-    
-                                System.out.println("\nPenilaian Pelayanan: ");
-                                System.out.println("1\t2\t3\t4\t5");
-                                System.out.print("\nNilai: ");
-                                Integer pilih = input.nextInt();
-                                System.out.print("\nMasukkan Komentar: ");
-                                String komen = input.next();
-                                list_komen.add("\nNama\t\t: "+name+"\nPenilaian\t:"+pilih+"\nKomentar\t: "+komen);
-                        
-                                System.out.println("\nNama\t\t: "+name+"\nPenilaian\t: "+pilih+"\nKomentar\t: "+komen);
-                                break;
-
-                                default:
-                                System.out.println("Pilihan tidak tersedia");
-                                break;
-                            }
                                 System.out.print("\nApakah Anda ingin melanjutkan [y/n]? ");
                                 pilihanUser = input.next();
                                 isContinue3 = pilihanUser.equalsIgnoreCase("y");
+                            
+                            }
+                               
                         }
                         
                        
@@ -186,18 +198,18 @@ public class App {
                     //PostOrder traversal
                     treeOrder.postOrder(root);
                     System.out.println();
-
                 break;
+
                 case "5":
                     System.out.println("Terima kasih (^ ^)");
                     isContinue = false;
                     break;
+                    
                 default:
                     System.out.println("Pilihan tidak tersedia");
                     break;
                 
             }
-
         }
  
     }
